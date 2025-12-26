eb9d97dc2124b112796365ee42e3c102ea37d397
viettask <viet.nguyen@viettask.com>
2025-12-27 07:37:48 +1000

    setting up first server

---
+++ hint.md
@@ -0,0 +1,7 @@
+In terminal:
+  npm init
+
+In package.json: 
+  "type": "module" => need to import node:http
+
+  const http = require 'node:http'
---
+++ server.js
@@ -0,0 +1,46 @@
+/*
+Challenge 1: 
+1. Initialise a nodejs project:
+	Name: “from-the-other-side”.
+    Description: “A platform for sharing ghostly encounters”.
+
+2. Enable modular js (in package.json).
+
+hint.md for help
+*/
+
+/*
+Challenge 2: 
+
+1.Set up a server that serves the string 
+    '<html><h1>The server is working</h1></html>'.
+    
+    What should the content type be? 
+    What status code should you send?
+
+2. Listen on port 8000 and log a connection message to the console.
+
+3. Open the browser to see your first served HTML.
+
+*/
+
+import http from 'node:http'
+
+const PORT=8000
+
+const server = http.createServer((req, res) =>{
+    //Set the status code 200 OK
+    res.statusCode = 200
+
+    //Set the content type to "text/html"
+    res.setHeader('Content-Type', 'text/html')
+
+    //send the HTML response
+    res.end(`<html><h1>The server is working</h1></html>`)
+})
+
+
+    //Listen on port 8000
+    server.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`)
+    })
